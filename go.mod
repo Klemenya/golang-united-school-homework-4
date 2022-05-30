0b003ed0e098a1d8047060a4e395521ee1d964e4
Pavel <klemenya@gmail.com>
2022-05-30 10:11:37 +0300

    master returned to original state

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module string_sum.go
 
-go 1.18
+go 1.13
--- string_sum.go
+++ string_sum.go
@@ -1,48 +1,27 @@
 package string_sum
 
 import (
-	"fmt"
 	"errors"
-	"regexp"
-	"strconv"
-	"strings"
 )
 
+//use these errors as appropriate, wrapping them with fmt.Errorf function
 var (
+	// Use when the input is empty, and input is considered empty if the string contains only whitespace
 	errorEmptyInput = errors.New("input is empty")
+	// Use when the expression has number of operands not equal to two
 	errorNotTwoOperands = errors.New("expecting two operands, but received more or less")
 )
 
+// Implement a function that computes the sum of two int numbers written as a string
+// For example, having an input string "3+5", it should return output string "8" and nil error
+// Consider cases, when operands are negative ("-3+5" or "-3-5") and when input string contains whitespace (" 3 + 5 ")
+//
+//For the cases, when the input expression is not valid(contains characters, that are not numbers, +, - or whitespace)
+// the function should return an empty string and an appropriate error from strconv package wrapped into your own error
+// with fmt.Errorf function
+//
+// Use the errors defined above as described, again wrapping into fmt.Errorf
 
 func StringSum(input string) (output string, err error) {
-	var errorEmptyInputMy = fmt.Errorf("Error_input_empty: %w", errorEmptyInput)
-	var errorPresentLetterMy = fmt.Errorf("Error: letter present ")
-	var errorInputMy = fmt.Errorf("Error_incorectly_input: %w", errorNotTwoOperands)
-
-	input = strings.ReplaceAll(input, " ", "") 
-
-	if len(strings.Fields(input)) == 0 {
-		return "", errorEmptyInputMy
-	}
-
-	isLatter, _ := regexp.MatchString("[A-Za-z]", input)
-	if isLatter != false {
-		return "", errorPresentLetterMy
-	}
-
-	isCorrect, _ := regexp.MatchString("^(\\-|\\+)?\\d+(\\-|\\+)\\d+$", input)
-	if isCorrect != true {
-		return "", errorInputMy
-	}
-
-	input = strings.ReplaceAll(input, "-", " -")
-	input = strings.ReplaceAll(input, "+", " +")
-	input = strings.TrimSpace(input)
-	res := strings.Split(input, " ")
-	x, _ := strconv.Atoi(res[0])
-	y, _ := strconv.Atoi(res[1])
-	output = strconv.Itoa(x + y)
-
-
-	return output, nil
+	return "", nil
 }
